b74487dc4c99d20544d3d3b3393294ff1db5eb6e
ddding <73097207+Ding-jinyu@users.noreply.github.com>
2024-08-13 10:31:05 +0800

    Delete src directory
    Trying to make it as a default branch

--- src/main/java/org/curastone/Crafty/dao/StepDao.java
+++
@@ -1,8 +0,0 @@
-package org.curastone.Crafty.dao;
-
-import org.curastone.Crafty.model.Step;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface StepDao extends MongoRepository<Step, String> {}
--- src/main/java/org/curastone/Crafty/dao/SubscriptionDao.java
+++
@@ -1,8 +0,0 @@
-package org.curastone.Crafty.dao;
-
-import org.curastone.Crafty.model.Subscription;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SubscriptionDao extends MongoRepository<Subscription, String> {}
